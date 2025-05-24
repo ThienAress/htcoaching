fcd5157015600458c632dfdaca60496f66e7c384
Your Name01~ <you@example.com>
2025-05-24 13:45:14 +0700

    responsive css done

--- my-create-app/src/App.jsx
+++ my-create-app/src/App.jsx
@@ -9,6 +9,7 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import ChatIcons from "./components/ChatIcons/ChatIcons";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
