eef5c5285bceccd9da518483a7fcf4a9f03378fa
Your Name01~ <you@example.com>
2025-05-30 12:34:21 +0700

    fix feedback and pricing on pc

--- my-create-app/src/App.jsx
+++ my-create-app/src/App.jsx
@@ -9,7 +9,6 @@ import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/ScrollToTop/ScrollToTop";
 import ChatIcons from "./components/ChatIcons/ChatIcons";
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
