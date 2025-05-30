4225fcd13f832e3baff2ecc2d75fe8dc7f3006e7
Your Name01~ <you@example.com>
2025-05-30 14:11:10 +0700

    code base done

--- my-create-app/src/components/ClassSection/Classes.jsx
+++ my-create-app/src/components/ClassSection/Classes.jsx
@@ -14,7 +14,8 @@ function Classes() {
           <ClassCard
             image="images/class3.jpg"
             title="Personal Training"
-            desc="Huấn luyện 1:1 giúp bạn theo sát tiến độ..."
+            desc="Huấn luyện 1:1 giúp bạn theo sát tiến độ, tập đúng kỹ thuật và
+                đạt mục tiêu nhanh hơn bao giờ hết."
             benefits={[
               "Lộ trình cá nhân hóa",
               "Tập trung vào mục tiêu riêng của bạn",
@@ -24,7 +25,8 @@ function Classes() {
           <ClassCard
             image="images/class2.jpg"
             title="Cardio & HIIT"
-            desc="Đốt cháy mỡ thừa, tăng nhịp tim..."
+            desc=" Đốt cháy mỡ thừa hiệu quả, tăng nhịp tim và cải thiện sức bền
+                chỉ trong vài phút mỗi buổi tập."
             benefits={[
               "Bài tập ngắn - Hiệu quả vượt trội",
               "Cải thiện sức khỏe tim mạch",
@@ -34,7 +36,8 @@ function Classes() {
           <ClassCard
             image="images/class4.jpg"
             title="Boxing"
-            desc="Tăng sức bền, cải thiện phản xạ..."
+            desc="Tăng sức bền, cải thiện phản xạ và giải phóng căng thẳng với
+                những bài tập đầy năng lượng."
             benefits={[
               "Đốt mỡ, săn chắc toàn thân",
               "Nâng cao phản xạ và sự tự tin",
